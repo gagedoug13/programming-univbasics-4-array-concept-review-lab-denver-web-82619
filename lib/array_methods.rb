f6438455d2f1c4d22157b9c42ad6c97d490819c9
Gage Thornberry <gagedoug13@gmail.com>
2019-08-14 20:14:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,10 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  p array.find_max_value()
+  array.length.times do |index|
+    puts array[index]
+
+  end
 end
 
 def find_min_value(array)
